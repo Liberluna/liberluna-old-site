b6fbdeaa64cdb8296dcce17ffaea7ec74213a10c
nakasyou <nakasyou1103@gmail.com>
2023-01-25 18:57:42 +0900

    fix

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 (async ()=>{
     document.getElementById('scroll-to').addEventListener("click",e=>{
-        scrollTo(0, window.innerHeight);
+        window.scroll({
+            top:document.getElementById("title-zone").offsetHeight,
+            behavior: 'smooth'
+        });
     });
 })();
