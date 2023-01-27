2f5a607b110e4a870c1b17d34c65db48137479ce
nakasyou <nakasyou1103@gmail.com>
2023-01-27 18:30:18 +0900

    change build script

--- build.js
+++ build.js
@@ -2,19 +2,24 @@ const ejs = require('ejs');
 const fs = require('fs');
 
 const files=JSON.parse(fs.readFileSync('map.json',"utf8")).files;
-for(const file of files){
-    console.log(file)
-    if(file.slice(-4)!==".ejs"){
-      console.log("not ejs")
-      fs.copyFile(`src/${file}`,`dist/${file}`,()=>{});
-      continue;
-    }
-    ejs.renderFile(`src/${file}`, {}, {
-        views:"./src",
-        strict:true
-    }, (err, str)=>{
-        fs.writeFile(`dist/${file.replaceAll('.ejs','.html')}`,str,(err)=>{
-            console.error(err);
+(async()=>{
+    for(const file of files){
+        console.log(file)
+        const dir=file.split("/").slice(0,-1).join("/");
+        if(!fs.existsSync(`dist/${dir}`)){
+            fs.mkdirSync(`dist/${dir}`,{ recursive:true})
+        }
+        if(file.slice(-4)!==".ejs"){
+          fs.copyFile(`src/${file}`,`dist/${file}`,()=>{});
+          continue;
+        }
+        ejs.renderFile(`src/${file}`, {}, {
+            views:"./src",
+            strict:true
+        }, (err, str)=>{
+            fs.writeFile(`dist/${file.replaceAll('.ejs','.html')}`,str,(err)=>{
+                console.error(err);
+            });
         });
-    });
-}
+    }
+})();
