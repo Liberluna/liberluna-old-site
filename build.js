2e7ce4d820de3c49cdb09d0f466b24456a081a27
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-01-28 17:00:07 +0900

    add auto makedir

--- build.js
+++ build.js
@@ -4,6 +4,11 @@ const fs = require('fs');
 const files=JSON.parse(fs.readFileSync('map.json',"utf8")).files;
 for(const file of files){
     console.log(file)
+    const dir=file.split("/").slice(0,-1).join("/");
+    if(!fs.existsSync(dir)){
+        console.log('make',dir);
+        fs.mkdirSync(dir,{recursive:true})
+    }
     if(file.slice(-4)!==".ejs"){
       console.log("not ejs")
       fs.copyFile(`src/${file}`,`dist/${file}`,()=>{});
