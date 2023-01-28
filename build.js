b3167c30bb30d74c3dc238dec05b485185a15930
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-01-28 17:17:54 +0900

    fix build.js

--- build.js
+++ build.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const files=JSON.parse(fs.readFileSync('map.json',"utf8")).files;
 for(const file of files){
     let dir=file.split("/").slice(0,-1).join("/");
-    if(dir===""){
-        dir="/";
+    if(dir[0]!=="/"){
+        dir="/"+dir;
     }
     const isdir=fs.existsSync(dir);
     console.log(file,dir,isdir)
     if(!isdir){
-        console.log('make',dir);
+        console.log('make','dist'+dir);
         fs.mkdirSync(dir,{recursive:true},()=>{})
     }
     if(file.slice(-4)!==".ejs"){
