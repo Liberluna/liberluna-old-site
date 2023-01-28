e0ce6c04c92874b801f0c2a8b7b5d91ed68d2f22
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-01-28 13:20:29 +0000

    build.js add img b64 encoder

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 const ejs = require('ejs');
 const fs = require('fs');
-
+const {JSDOM}=require('jsdom')
+const mime = require('mime-types')
 
 const build=async()=>{
     const files=JSON.parse(fs.readFileSync('map.json',"utf8")).files;
     for(const file of files){
-        //console.log(file)
         const dir=file.split("/").slice(0,-1).join("/");
         if(!fs.existsSync(`dist/${dir}`)){
             fs.mkdirSync(`dist/${dir}`,{ recursive:true})
@@ -18,8 +18,16 @@ const build=async()=>{
             views:"./src",
             strict:true
         }, (err, str)=>{
+            const dom=new JSDOM(str);
+            dom.window.document.querySelectorAll('img').forEach(elem=>{
+                const imgpath=`./src/${elem.src}`;
+                const b64txt=fs.readFileSync(imgpath,{encoding:"base64"});
+                const b64url=`data:${mime.lookup(imgpath)};base64,${b64txt}`;
+                elem.src=b64url;
+            });
+            str=dom.window.document.documentElement.outerHTML;
             fs.writeFile(`dist/${file.replaceAll('.ejs','.html')}`,str,(err)=>{
-                //console.error(err);
+              if(err) console.error(err);
             });
         });
     }
