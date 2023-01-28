784506b0c5fc4df7e1ed9367846bc5915dc6f5de
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-01-28 17:22:59 +0900

    change abspath

--- build.js
+++ build.js
@@ -7,22 +7,22 @@ for(const file of files){
     if(dir[0]!=="/"){
         dir="/"+dir;
     }
-    const isdir=fs.existsSync('dist'+dir);
+    const isdir=fs.existsSync('./dist'+dir);
     console.log(file,dir,isdir)
     if(!isdir){
-        console.log('make','dist'+dir);
+        console.log('make','./dist'+dir);
         fs.mkdirSync(dir,{recursive:true},()=>{})
     }
     if(file.slice(-4)!==".ejs"){
       //console.log("not ejs")
-      fs.copyFile(`src/${file}`,`dist/${file}`,()=>{});
+      fs.copyFile(`src/${file}`,`./dist/${file}`,()=>{});
       continue;
     }
     ejs.renderFile(`src/${file}`, {}, {
         views:"./src",
         strict:true
     }, (err, str)=>{
-        fs.writeFile(`dist/${file.replaceAll('.ejs','.html')}`,str,(err)=>{
+        fs.writeFile(`./dist/${file.replaceAll('.ejs','.html')}`,str,(err)=>{
             console.error(err);
         });
     });
