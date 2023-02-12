d7e3f8d6a0a81eb7aaf240d4719840273f44b5b4
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-02-12 22:10:05 +0900

    fi

--- builder/html-b64.ts
+++ builder/html-b64.ts
@@ -10,8 +10,7 @@ export default function(code: string,file):string{
   doc.querySelectorAll('img').forEach(elem=>{
     console.log(elem.getAttribute('src'))
     if(elem.getAttribute("src")){
-      const base=new URL("./"+file.dir,cwdURL).href;
-      console.log(base)
+      const base=new URL("./"+file.dir,cwdURL);
       const path=new URL(elem.getAttribute("src"),base);
       console.log(path)
       const b64=encode(Deno.readFileSync(path));
