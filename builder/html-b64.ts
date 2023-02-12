ec34387e01cd3f26f5b62c199f3abc798e70824b
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-02-13 08:49:40 +0900

    fix

--- builder/html-b64.ts
+++ builder/html-b64.ts
@@ -9,7 +9,7 @@ export default function(code: string,file):string{
   doc.querySelectorAll('img').forEach(elem=>{
     console.log(elem.getAttribute('src'))
     if(elem.getAttribute("src")){
-      const path=file.dir+''+elem.getAttribute('src')
+      const path=file.dir+'/'+elem.getAttribute('src')
       console.log(path)
       const b64=encode(Deno.readFileSync(path));
       const dataurl=`data:${mime.getType(path)};base64,${b64}`;
