ae0e6aa88a8efc1d3c7527972ea447f7dc8931c3
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-02-17 08:45:43 +0000

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('default',()=>{
     
     gulp.src('./src/**/*.ts')
       .pipe(typescript({
-        lib:["es2021"],
+        lib:["es2021","DOM"],
         allowJs:true
       }))
       .pipe(gulp.dest('./dist')),
