eff503b10ae61fc2863e382dcd6c6c0ef7ac0111
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-02-21 19:34:53 +0900

    checkbottunディレクトリの削除

--- checkbottun/index.js
+++
@@ -1,8 +0,0 @@
-$(".toggle").on("click", function() {
-    $(".toggle").toggleClass("checked");
-    if(!$('input[name="check"]').prop("checked")) {
-      $(".toggle input").prop("checked", true);
-    } else {
-      $(".toggle input").prop("checked", false);
-    }
-  });
